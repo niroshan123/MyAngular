b4aeb35d6fdc5cc465d16c7532ad9e78ef791bfe
niroshan123 <niroshanratnayake07@gmail.com>
2019-08-31 11:32:57 +0530

    components

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { RedComponent } from './red/red.component';
+import { GreenComponent } from './green/green.component';
+import { YellowComponent } from './yellow/yellow.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RedComponent,
+    GreenComponent,
+    YellowComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
